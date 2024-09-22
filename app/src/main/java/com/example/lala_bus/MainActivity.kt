ea836b47aa301cae4655e7406e6f0ba7a64dd733
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-09-22 09:08:06 +0800

    finished setting up the fragments for bottom nav bar (no designs and others yet)

--- app/src/main/java/com/example/lala_bus/MainActivity.kt
+++ app/src/main/java/com/example/lala_bus/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.lala_bus.databinding.ActivityMainBinding
+import com.example.lala_bus.main_fragments.HomeFragment
+import com.example.lala_bus.main_fragments.StationFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,5 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         // Code Start
 
+        replaceFragment(HomeFragment()) // Main Start of the app is to go to Home Fragment
+
+        // Bottom Navigation Bar Fragment Replacement
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.stations -> replaceFragment(StationFragment())
+                else -> {
+                    replaceFragment(HomeFragment())
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
