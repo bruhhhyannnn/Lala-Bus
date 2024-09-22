52c761518482a6e6e2076c09f249c4dc1afe8509
bruhhhyannnn <bryanjesusmangapit477@gmail.com>
2024-09-22 15:38:06 +0800

    added basic maps in fragment_home.xml (camera zoom and marker addition are both not working)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.map.secret)
 }
 
 android {
@@ -45,6 +46,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.google.maps)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
--- app/src/main/java/com/example/lala_bus/MainActivity.kt
+++ app/src/main/java/com/example/lala_bus/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.lala_bus
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.lala_bus.databinding.ActivityMainBinding
 import com.example.lala_bus.main_fragments.HomeFragment
 import com.example.lala_bus.main_fragments.StationFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding : ActivityMainBinding
 
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         // Code Start
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
 
         replaceFragment(HomeFragment()) // Main Start of the app is to go to Home Fragment
 
@@ -35,7 +42,6 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -43,5 +49,23 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.main_frame_layout, fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val latitudeLongitude = LatLng(18.057205, 120.548080)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latitudeLongitude, 15f)) // not working
+        map.addMarker(com.google.android.gms.maps.model.MarkerOptions().position(latitudeLongitude).title("Marker")) // not working
+
+        // Possible Additions:
+        map.uiSettings.isZoomGesturesEnabled = true
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isCompassEnabled = true
+        map.uiSettings.isRotateGesturesEnabled = true
+        map.uiSettings.isScrollGesturesEnabled = true
+        map.uiSettings.isTiltGesturesEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
+        map.uiSettings.isMapToolbarEnabled = true
+        map.uiSettings.isIndoorLevelPickerEnabled = true
+
     }
 }
