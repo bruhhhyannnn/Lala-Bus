5b62c15ad63827ceaaa1a554ba3886425bd536e4
Bryan Jesus Mangapit <bryanjesusmangapit477@gmail.com>
2024-09-23 09:53:53 +0800

    added SplashArtWelcomeActivity (progress bar doesn't change to white color)

--- app/src/main/java/com/example/lala_bus/MainActivity.kt
+++ app/src/main/java/com/example/lala_bus/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.lala_bus
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.lala_bus.databinding.ActivityMainBinding
 import com.example.lala_bus.main_fragments.HomeFragment
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // Code Start
 
---
+++ app/src/main/java/com/example/lala_bus/starters/SplashArtWelcomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lala_bus.starters
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lala_bus.MainActivity
+import com.example.lala_bus.R
+
+class SplashArtWelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_art)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashArtWelcomeActivity, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
