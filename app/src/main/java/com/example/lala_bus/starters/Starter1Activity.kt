008cc4cc6de5d5895f5988b82436ac99d5b26a6c
Bryan Jesus Mangapit <bryanjesusmangapit477@gmail.com>
2024-09-23 11:44:06 +0800

    added Starter1Activity (not added button functionalities yet, most design implemented)

--- app/src/main/java/com/example/lala_bus/starters/Starter1Activity.kt
+++ app/src/main/java/com/example/lala_bus/starters/Starter1Activity.kt
@@ -1,32 +1,24 @@
 package com.example.lala_bus.starters
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.lala_bus.R
 
-class SplashArtWelcomeActivity : AppCompatActivity() {
+class Starter1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_art)
+        setContentView(R.layout.activity_starter1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        startActivity(Intent(this@SplashArtWelcomeActivity, Starter1Activity::class.java))
-        finish()
+        // Code
 
-        // Commenting this for now so that it is faster to develop
-//        Handler().postDelayed({
-//            startActivity(Intent(this@SplashArtWelcomeActivity, Starter1Activity::class.java))
-//            finish()
-//        }, 1000)
     }
 }
