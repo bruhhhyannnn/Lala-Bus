19c2bf1d61bc151804a253f14593ee90d6973aa0
Bryan Mangapit <mangapit.bryan@gmail.com>
2024-09-28 17:39:39 +0800

    updated & added rotation stop points on Laoag-Paoay and Paoay-Laoag (lat & long)

--- app/src/main/java/com/example/lala_bus/main_fragments/HomeFragment.kt
+++ app/src/main/java/com/example/lala_bus/main_fragments/HomeFragment.kt
@@ -110,7 +110,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         // Map Actions
         map.uiSettings.isCompassEnabled = false
         map.uiSettings.isZoomControlsEnabled = true
-//        map.mapType = GoogleMap.MAP_TYPE_HYBRID  // i will fix this someday to use map type hybrid for betterness
+        // map.mapType = GoogleMap.MAP_TYPE_HYBRID  // i will fix this someday to use map type hybrid for betterness
         map.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.map_style)) // the mapType is overwrite by this kase
 
         // Default Location
@@ -118,64 +118,77 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLatitudeLongitude, 16f))
 
         // Get station and stop point data
-        val (modernJeepStations, rotationStopPoint_PaoayLaoag, rotationStopPoint_LaoagPaoay) = getStationsLatLong()
+        val (modernJeepStations, rotationStopPoint_LaoagPaoay, rotationStopPoint_PaoayLaoag) = getStationsLatLong()
 
         // Add markers to the map
         addMarkersToMap(map, modernJeepStations) // adding modern jeep stations marker
-        addMarkersToMap(map, rotationStopPoint_PaoayLaoag) // adding rotation stop points marker for Paoay-Batac-Laoag rotation
         addMarkersToMap(map, rotationStopPoint_LaoagPaoay) // adding rotation stop points marker for Laoag-Batac-Paoay rotation
+        addMarkersToMap(map, rotationStopPoint_PaoayLaoag) // adding rotation stop points marker for Paoay-Batac-Laoag rotation
     }
 
     private fun getStationsLatLong(): Triple<List<MarkerData>, List<MarkerData>, List<MarkerData>> {
         // Modern Jeep Permanent Stations
         val modernJeepStations = listOf(
             MarkerData(LatLng(18.195729, 120.590441), "Laoag Modern Jeep Station"),
-            MarkerData(LatLng(18.060065, 120.520211), "Paoay Modern Jeep Station")
-        )
-
-        // Rotation Stop Points (Paoay-Batac-Laoag)
-        val rotationStopPoint_PaoayLaoag = listOf(
-            MarkerData(LatLng(18.060268, 120.521710), "7-Eleven, Paoay"),
-            MarkerData(LatLng(18.061411, 120.522567), "Paoay Pick-Up Point (1)"),
-            MarkerData(LatLng(18.061497, 120.524002), "Paoay Pick-Up Point (2)"),
-            MarkerData(LatLng(18.057777, 120.543879), "Shortcut COE MMSU, Batac"),
-            MarkerData(LatLng(18.057144, 120.547645), "Twin Gate MMSU, Batac"),
-            MarkerData(LatLng(18.056291, 120.552822), "Teatro MMSU, Batac"),
-            MarkerData(LatLng(18.055938, 120.555326), "Gate 3 MMSU, Batac"),
-            MarkerData(LatLng(18.0556, 120.5575), "Crossing (Tealive), Batac"),
-            MarkerData(LatLng(18.054762, 120.562627), "Jollibee, Batac"),
-            MarkerData(LatLng(18.054760, 120.564350), "Centro (Empanadahan) Batac"),
-            MarkerData(LatLng(18.066417, 120.562268), "Market Place, Batac"),
-            MarkerData(LatLng(18.108253, 120.568959), "Baay Market Place, Batac"),
-            MarkerData(LatLng(18.172889, 120.594202), "San Nicolas Municipality"),
-            MarkerData(LatLng(18.179488, 120.590411), "Robinsons, San Nicolas"),
-            MarkerData(LatLng(18.185368, 120.588945), "Gabu Terminal, San Nicolas"),
-            MarkerData(LatLng(18.196158, 120.592418), "Laoag City Municipality")
+            MarkerData(LatLng(18.051355, 120.518746), "Paoay Modern Jeep Station")
         )
 
         // Rotation Stop Points (Laoag-Batac-Paoay)
         val rotationStopPoint_LaoagPaoay = listOf(
-            MarkerData(LatLng(18.185338, 120.588861), "Gabu Terminal, San Nicolas"),
-            MarkerData(LatLng(18.179402, 120.590341), "Robinsons, San Nicolas"),
-            MarkerData(LatLng(18.172419, 120.593810), "7-Eleven, San Nicolas"),
-            MarkerData(LatLng(18.140296, 120.584247), "Unknown Place"),
-            MarkerData(LatLng(18.136200, 120.583531), "Bingao Elementary & National High School, San Nicolas"),
-            MarkerData(LatLng(18.128695, 120.579486), "Brgy. Bingao, San Nicolas"),
-            MarkerData(LatLng(18.115858, 120.572473), "Baay Elementary School, Batac"),
-            MarkerData(LatLng(18.111380, 120.570396), "Batac National High School, Batac"),
-            MarkerData(LatLng(18.108161, 120.568836), "Baay Market Place, Batac"),
-            MarkerData(LatLng(18.088820, 120.568140), "GARSH and Bil-loca Elementary School, Batac"),
-            MarkerData(LatLng(18.079645, 120.563124), "Rana-Ann Gas Station, Baligat, Batac"),
+            MarkerData(LatLng(18.186152, 120.588687), "Before Bridge of Laoag City"),
+            MarkerData(LatLng(18.179402, 120.590341), "Robinsons Ilocos, San Nicolas"),
+            MarkerData(LatLng(18.172783, 120.593965), "San Nicolas Municipality"),
+            MarkerData(LatLng(18.153305, 120.585531), "Cockfight Arena, San Nicolas"),
+            MarkerData(LatLng(18.140267, 120.584148), "San Lorenzo, San Nicolas"),
+            MarkerData(LatLng(18.136204, 120.583580), "Bingao Elementary & National High School, San Nicolas"),
+            MarkerData(LatLng(18.128952, 120.579599), "Brgy. Bingao, San Nicolas"),
+            MarkerData(LatLng(18.116064, 120.572574), "Baay Elementary School, Batac"),
+            MarkerData(LatLng(18.111539, 120.570348), "Batac National High School, Batac"),
+            MarkerData(LatLng(18.108161, 120.568836), "Market Place, Brgy. Baay, Batac"),
+            MarkerData(LatLng(18.088731, 120.568068), "GARSH and Bil-loca Elementary School, Batac"),
+            MarkerData(LatLng(18.079227, 120.562914), "Rana-Ann Gas Station, Baligat, Batac"),
             MarkerData(LatLng(18.067465, 120.561356), "Arko Batac"),
-            MarkerData(LatLng(18.065592, 120.562959), "Market Place, Batac"),
-            MarkerData(LatLng(18.0567, 120.5647), "Pik A Bun, Batac"),
-            MarkerData(LatLng(18.055136, 120.561324), "Cebuana Pawnshop, Batac"),
-            MarkerData(LatLng(18.056060, 120.555191), "Gate 3 MMSU, Batac"),
-            MarkerData(LatLng(18.056414, 120.552752), "Teatro MMSU, Batac"),
-            MarkerData(LatLng(18.057171, 120.548076), "Twin Gate MMSU, Batac"),
-            MarkerData(LatLng(18.057863, 120.544007), "Shortcut COE MMSU, Batac"),
+            MarkerData(LatLng(18.065589, 120.562940), "Market Place, Batac"),
+            MarkerData(LatLng(18.0567, 120.5647), "Pik A Bun (Centro), Batac"),
+            MarkerData(LatLng(18.055136, 120.561324), "Cebuana Pawnshop (Centro), Batac"),
+            MarkerData(LatLng(18.055791, 120.557112), "Eco-Oil Gas Station (Crossing), Batac"),
+            MarkerData(LatLng(18.056060, 120.555191), "MMSU Gate 3, Batac"),
+            MarkerData(LatLng(18.056383, 120.552828), "MMSU Teatro, Batac"),
+            MarkerData(LatLng(18.057171, 120.548076), "MMSU Twin Gate, Batac"),
+            MarkerData(LatLng(18.057863, 120.544007), "MMSU COE Shortcut, Batac"),
+            MarkerData(LatLng(18.061506, 120.523962), "Salbang, Paoay"),
             MarkerData(LatLng(18.061527, 120.522527), "Paoay Church"),
             MarkerData(LatLng(18.060370, 120.521639), "Paoay Municipality")
+        )
+
+        // Rotation Stop Points (Paoay-Batac-Laoag)
+        val rotationStopPoint_PaoayLaoag = listOf(
+            MarkerData(LatLng(18.060268, 120.521710), "7-Eleven (Centro), Paoay"),
+            MarkerData(LatLng(18.061411, 120.522567), "Paoay Church"),
+            MarkerData(LatLng(18.061446, 120.524006), "Salbang, Paoay"),
+            MarkerData(LatLng(18.057726, 120.544146), "Phil-Rice Research Institute, Batac"),
+            MarkerData(LatLng(18.057144, 120.547645), "MMSU Twin Gate, Batac"),
+            MarkerData(LatLng(18.056291, 120.552822), "MMSU Teatro, Batac"),
+            MarkerData(LatLng(18.055938, 120.555326), "MMSU Gate 3, Batac"),
+            MarkerData(LatLng(18.0556, 120.5575), "Crossing (Tealive), Batac"),
+            MarkerData(LatLng(18.054762, 120.562627), "Jollibee, Batac"),
+            MarkerData(LatLng(18.054760, 120.564350), "In-Front of City of Batac Municipality"),
+            MarkerData(LatLng(18.056616, 120.565189), "Ricarte Park, Batac"),
+            MarkerData(LatLng(18.066417, 120.562268), "Market Place, Batac"),
+            MarkerData(LatLng(18.078790, 120.563053), "Rana-Ann Gas Station, Baligat, Batac"),
+            MarkerData(LatLng(18.068090, 120.561330), "Arko Batac"),
+            MarkerData(LatLng(18.088505, 120.568151), "GARSH and Bil-loca Elementary School, Batac"),
+            MarkerData(LatLng(18.108253, 120.568959), "Market Place, Brgy. Baay, Batac"),
+            MarkerData(LatLng(18.111380, 120.570396), "Batac National High School, Batac"),
+            MarkerData(LatLng(18.115854, 120.572622), "Baay Elementary School, Batac"),
+            MarkerData(LatLng(18.128701, 120.579635), "Brgy. Bingao, San Nicolas"),
+            MarkerData(LatLng(18.136172, 120.583707), "Bingao Elementary & National High School, San Nicolas"),
+            MarkerData(LatLng(18.140290, 120.584265), "San Lorenzo, San Nicolas"),
+            MarkerData(LatLng(18.152961, 120.585532), "Cockfight Arena, San Nicolas"),
+            MarkerData(LatLng(18.172889, 120.594202), "San Nicolas Municipality"),
+            MarkerData(LatLng(18.179488, 120.590411), "Robinsons Ilocos, San Nicolas"),
+            MarkerData(LatLng(18.185368, 120.588945), "Gabu Terminal, San Nicolas"),
+            MarkerData(LatLng(18.196158, 120.592418), "Laoag City Municipality")
         )
 
         return Triple(modernJeepStations, rotationStopPoint_PaoayLaoag, rotationStopPoint_LaoagPaoay)
